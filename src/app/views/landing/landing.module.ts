43891f4fab1c2d36d640f61244e0c38af5d3bb75
Luis Cabrera <gigocabrera@outlook.com>
2020-12-22 10:24:34 -0500

    removed unwanted pages

--- src/app/views/landing/landing.module.ts
+++ src/app/views/landing/landing.module.ts
@@ -7,7 +7,6 @@ import { CommonModule } from '@angular/common';
 import { NguCarouselModule } from "@ngu/carousel";
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { LandingRoutingModule } from './landing-routing.module';
-import { LandingV1Component } from './landing-v1/landing-v1.component';
 import { Intro1Component } from './components/intro1/intro1.component';
 import { Intro2Component } from './components/intro2/intro2.component';
 import { Works1Component } from './components/works1/works1.component';
@@ -31,30 +30,21 @@ import { NewsComponent } from './components/news/news.component';
 import { FaqsComponent } from './components/faqs/faqs.component';
 import { NewsTwoComponent } from './components/news-two/news-two.component';
 import { IntroThreeComponent } from './components/intro-three/intro-three.component';
-import { LandingV3Component } from './landing-v3/landing-v3.component';
 import { IntroFourComponent } from './components/intro-four/intro-four.component';
-import { LandingV4Component } from './landing-v4/landing-v4.component';
 import { IntroFiveComponent } from './components/intro-five/intro-five.component';
-import { LandingV5Component } from './landing-v5/landing-v5.component';
-import { LandingV6Component } from './landing-v6/landing-v6.component';
 import { IntroSixComponent } from './components/intro-six/intro-six.component';
 import { IntroSevenComponent } from './components/intro-seven/intro-seven.component';
-import { LandingV7Component } from './landing-v7/landing-v7.component';
 import { IntroEightComponent } from './components/intro-eight/intro-eight.component';
-import { LandingV8Component } from './landing-v8/landing-v8.component';
 import { HeaderComponent } from './components/header/header.component';
 import { HeaderWhiteComponent } from './components/header-white/header-white.component';
 import { IntroNineComponent } from './components/intro-nine/intro-nine.component';
-import { LandingV9Component } from './landing-v9/landing-v9.component';
 import { IntroTenComponent } from './components/intro-ten/intro-ten.component';
-import { LandingV10Component } from './landing-v10/landing-v10.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { FormsModule } from '@angular/forms';
 import { DemosComponent } from './components/demos/demos.component';
 import { BlogDetailsComponent } from './components/blog-details/blog-details.component';
 import { BlogDetailsPageComponent } from './blog-details-page/blog-details-page.component';
 import { IntroElevenComponent } from './components/intro-eleven/intro-eleven.component';
-import { LandingV11Component } from './landing-v11/landing-v11.component';
 
 @NgModule({
   imports: [
@@ -66,7 +56,6 @@ import { LandingV11Component } from './landing-v11/landing-v11.component';
     FormsModule
   ],
   declarations: [
-    LandingV1Component,
     Intro1Component,
     Intro2Component,
     Works1Component,
@@ -90,29 +79,20 @@ import { LandingV11Component } from './landing-v11/landing-v11.component';
     FaqsComponent,
     NewsTwoComponent,
     IntroThreeComponent,
-    LandingV3Component,
     IntroFourComponent,
-    LandingV4Component,
     IntroFiveComponent,
-    LandingV5Component,
-    LandingV6Component,
     IntroSixComponent,
     IntroSevenComponent,
-    LandingV7Component,
     IntroEightComponent,
-    LandingV8Component,
     HeaderComponent,
     ScrollToDirective,
     HeaderWhiteComponent,
     IntroNineComponent,
-    LandingV9Component,
     IntroTenComponent,
-    LandingV10Component,
     DemosComponent,
     BlogDetailsComponent,
     BlogDetailsPageComponent,
-    IntroElevenComponent,
-    LandingV11Component,
+    IntroElevenComponent
 
   ],
   providers: [WINDOW_PROVIDERS]
