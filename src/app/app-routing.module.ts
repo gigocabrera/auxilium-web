6c3f1334434678f8171b17b3d6f376687e8b397e
Luis Cabrera <gigocabrera@outlook.com>
2020-12-31 07:58:58 -0500

    cleaned route to display V2 view as default page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,27 +5,17 @@ import {Routes, RouterModule} from "@angular/router";
 import {AuthLayoutComponent} from "./shared/components/layouts/auth-layout/auth-layout.component";
 
 const routes: Routes = [
-  {
+  /* {
     path: "",
     redirectTo: "welcome",
     pathMatch: "full",
-  },
-  /* {
-    path: "",
-    component: AuthLayoutComponent,
-    children: [
-      {
-        path: "sessions",
-        loadChildren: () => import("./views/sessions/sessions.module").then((m) => m.SessionsModule),
-      },
-    ],
   }, */
   {
     path: "",
     component: LandingLayoutComponent,
     children: [
       {
-        path: "welcome",
+        path: "",
         loadChildren: () => import("./views/landing/landing.module").then((m) => m.LandingModule),
       },
     ],
