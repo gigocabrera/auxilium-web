e890f2046a23e15db3fa309f47cb9818d90dedc4
IamDre <a14cabrera@gmail.com>
2021-02-04 10:56:37 -0500

    cleaned up routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,9 @@ import { PreloaderComponent } from './components/layouts/preloader/preloader.com
 import { FooterComponent } from './components/layouts/footer/footer.component';
 import { NavbarComponent } from './components/layouts/navbar/navbar.component';
 import { StoryComponent } from './components/layouts/story/story.component';
-import { HomeOneComponent } from './components/pages/home-one/home-one.component';
 import { TeamComponent } from './components/layouts/team/team.component';
 import { TestimonialComponent } from './components/layouts/testimonial/testimonial.component';
 import { CtaComponent } from './components/layouts/cta/cta.component';
-import { HomeTwoComponent } from './components/pages/home-two/home-two.component';
 import { CallToActionComponent } from './components/layouts/call-to-action/call-to-action.component';
 import { SubscribeComponent } from './components/layouts/subscribe/subscribe.component';
 import { ContactComponent } from './components/layouts/contact/contact.component';
@@ -29,23 +27,6 @@ import { BlogComponent } from './components/layouts/blog/blog.component';
 import { CtaTwoComponent } from './components/layouts/cta-two/cta-two.component';
 import { WhoWeAreComponent } from './components/layouts/who-we-are/who-we-are.component';
 import { FunfactsTwoComponent } from './components/layouts/funfacts-two/funfacts-two.component';
-import { HomeThreeComponent } from './components/pages/home-three/home-three.component';
-import { HomeFourComponent } from './components/pages/home-four/home-four.component';
-import { HomeFiveComponent } from './components/pages/home-five/home-five.component';
-import { HomeSixComponent } from './components/pages/home-six/home-six.component';
-import { HomeSevenComponent } from './components/pages/home-seven/home-seven.component';
-import { HomeEightComponent } from './components/pages/home-eight/home-eight.component';
-import { HomeNineComponent } from './components/pages/home-nine/home-nine.component';
-import { HomeTenComponent } from './components/pages/home-ten/home-ten.component';
-import { HomeTwelveComponent } from './components/pages/home-twelve/home-twelve.component';
-import { HomeThirteenComponent } from './components/pages/home-thirteen/home-thirteen.component';
-import { HomeFourteenComponent } from './components/pages/home-fourteen/home-fourteen.component';
-import { HomeFifteenComponent } from './components/pages/home-fifteen/home-fifteen.component';
-import { HomeSixteenComponent } from './components/pages/home-sixteen/home-sixteen.component';
-import { HomeSeventeenComponent } from './components/pages/home-seventeen/home-seventeen.component';
-import { HomeEighteenComponent } from './components/pages/home-eighteen/home-eighteen.component';
-import { HomeNineteenComponent } from './components/pages/home-nineteen/home-nineteen.component';
-import { HomeTwentyComponent } from './components/pages/home-twenty/home-twenty.component';
 import { BlogOneComponent } from './components/pages/blog-one/blog-one.component';
 import { BlogTwoComponent } from './components/pages/blog-two/blog-two.component';
 import { BlogDetailsComponent } from './components/pages/blog-details/blog-details.component';
@@ -61,11 +42,9 @@ import { AuxHomeComponent } from './components/pages/aux-home/aux-home.component
     FooterComponent,
     NavbarComponent,
     StoryComponent,
-    HomeOneComponent,
     TeamComponent,
     TestimonialComponent,
     CtaComponent,
-    HomeTwoComponent,
     CallToActionComponent,
     SubscribeComponent,
     ContactComponent,
@@ -83,23 +62,6 @@ import { AuxHomeComponent } from './components/pages/aux-home/aux-home.component
     CtaTwoComponent,
     WhoWeAreComponent,
     FunfactsTwoComponent,
-    HomeThreeComponent,
-    HomeFourComponent,
-    HomeFiveComponent,
-    HomeSixComponent,
-    HomeSevenComponent,
-    HomeEightComponent,
-    HomeNineComponent,
-    HomeTenComponent,
-    HomeTwelveComponent,
-    HomeThirteenComponent,
-    HomeFourteenComponent,
-    HomeFifteenComponent,
-    HomeSixteenComponent,
-    HomeSeventeenComponent,
-    HomeEighteenComponent,
-    HomeNineteenComponent,
-    HomeTwentyComponent,
     BlogOneComponent,
     BlogTwoComponent,
     BlogDetailsComponent,
